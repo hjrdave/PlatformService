5991670df31519b2b15a287f0a2d74d612a687cb
David Sanders <dsanders_m633@outlook.com>
2022-04-06 09:46:25 -0400

    mac compatible template

--- SyncDataServices/Http/ICommandDataClient.cs
+++
@@ -1,8 +0,0 @@
-using PlatformService.Dtos;
-namespace PlatformService.SyncDataServices.Http
-{
-    public interface ICommandDataClient
-    {
-        Task SendPlatformToCommand(PlateformReadDtos platformread);
-    }
-}
